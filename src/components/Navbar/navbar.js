d9f6dfd3071408d0271313703661ce52b319ef34
NJSouza69 <njsouza.cfg@gmail.com>
2021-11-17 13:13:58 -0300

    Primeiro commit

---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./navbar.css"
+export default function NavBar() {
+    return (
+        <nav className="navbar">
+            <span>React Kanban</span>
+        </nav>
+    );
+}
